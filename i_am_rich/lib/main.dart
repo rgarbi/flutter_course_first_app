11ce0cae942c51a34db3e11339bed2f485d90ce6
Richard Garbi <richard@mobilgov.com>
2020-09-03 05:34:10 -0500

    change some colors

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -8,8 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
       ),
     );
   }
