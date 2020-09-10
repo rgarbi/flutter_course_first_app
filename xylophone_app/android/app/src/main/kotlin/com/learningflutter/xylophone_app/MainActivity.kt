36b43db0c0188d58370c748eefe13ff54f69ef60
Richard Garbi <richard@mobilgov.com>
2020-09-10 06:28:18 -0500

    the next app

---
+++ xylophone_app/android/app/src/main/kotlin/com/learningflutter/xylophone_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.learningflutter.xylophone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ xylophone_app/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    play(int soundNumber) {
+      final audioPlayer = AudioCache();
+      audioPlayer.play('note$soundNumber.wav');
+    }
+
+    Widget buildKey(int soundKey, MaterialColor color) {
+      return Expanded(
+        child: FlatButton(
+          onPressed: () {
+            play(soundKey);
+          },
+          color: color,
+        ),
+      );
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(1, Colors.red),
+              buildKey(2, Colors.orange),
+              buildKey(3, Colors.yellow),
+              buildKey(4, Colors.green),
+              buildKey(5, Colors.teal),
+              buildKey(6, Colors.blue),
+              buildKey(7, Colors.purple),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
