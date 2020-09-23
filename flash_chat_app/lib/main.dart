1f5d26e4d4ef6d89698270abfe33da7c56d21ea8
Richard Garbi <richard@mobilgov.com>
2020-09-23 09:57:07 -0500

    new app

---
+++ flash_chat_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flash_chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
