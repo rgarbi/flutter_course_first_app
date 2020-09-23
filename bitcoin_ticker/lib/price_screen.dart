70ed6fb5ef656ccc683dd8a62c54051a10d4f094
Richard Garbi <richard@mobilgov.com>
2020-09-23 09:32:41 -0500

    my impl

--- bitcoin_ticker/lib/price_screen.dart
+++ bitcoin_ticker/lib/price_screen.dart
@@ -5,6 +5,14 @@ import 'package:bitcoin_ticker/services/coin_api_service.dart';
 import 'package:bitcoin_ticker/services/coin_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const doubleBounce = Center(
+  child: SpinKitDoubleBounce(
+    color: Colors.black,
+    size: 50.0,
+  ),
+);
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -15,6 +23,7 @@ class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
   String selectedCryptoCurrency = 'BTC';
   ExchangeRate exchangeRate;
+  List<Widget> exchangeCards;
 
   DropdownButton<String> androidDropDown() {
     return DropdownButton<String>(
@@ -22,6 +31,7 @@ class _PriceScreenState extends State<PriceScreen> {
         setState(() {
           selectedCurrency = value;
         });
+        getAllPriceCards(selectedCurrency);
       },
       items: CoinData().getCurrencyListAsDropdownMenuItems(),
       value: selectedCurrency,
@@ -32,34 +42,79 @@ class _PriceScreenState extends State<PriceScreen> {
     return CupertinoPicker(
       itemExtent: 32.0,
       onSelectedItemChanged: (selectedIndex) {
-        selectedCurrency = currenciesList[selectedIndex];
-        getExchangeRate();
+        setState(() {
+          selectedCurrency = currenciesList[selectedIndex];
+        });
+        getAllPriceCards(selectedCurrency);
       },
       children: CoinData().getCurrencyListAsTextWidgets(),
     );
   }
 
-  Future<void> getExchangeRate() async {
-    ExchangeRate rate = await CoinApiService()
+  Future<ExchangeRate> getExchangeRate(
+      selectedCurrency, selectedCryptoCurrency) async {
+    return await CoinApiService()
         .getExchangeRate(selectedCurrency, selectedCryptoCurrency);
-
-    setState(() {
-      exchangeRate = rate;
-    });
   }
 
-  String getFinalQuoteText() {
+  String getFinalQuoteText(
+      ExchangeRate exchangeRate, currency, cryptoCurrency) {
     if (exchangeRate != null) {
-      return '1 $selectedCryptoCurrency = ${exchangeRate.rate.toStringAsFixed(2)} $selectedCurrency';
+      return '1 $cryptoCurrency = ${exchangeRate.rate.toStringAsFixed(2)} $currency';
     } else {
-      return '1 $selectedCryptoCurrency = 🤷 $selectedCurrency';
+      return '1 $cryptoCurrency = 🤷 $currency';
     }
+  }
+
+  void getAllPriceCards(selectedCurrency) async {
+    List<Widget> cards = [];
+
+    for (String crypto in cryptoList) {
+      ExchangeRate exchangeRate =
+          await getExchangeRate(selectedCurrency, crypto);
+
+      Padding currencyCard = Padding(
+        padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+        child: Card(
+          color: Colors.lightBlueAccent,
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+            child: Text(
+              getFinalQuoteText(exchangeRate, crypto, selectedCurrency),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      );
+
+      cards.add(currencyCard);
+    }
+
+    cards.add(Container(
+      height: 150.0,
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(bottom: 30.0),
+      color: Colors.lightBlue,
+      child: Platform.isIOS ? iOSPicker() : androidDropDown(),
+    ));
+
+    setState(() {
+      exchangeCards = cards;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getExchangeRate();
+    getAllPriceCards(selectedCurrency);
   }
 
   @override
@@ -71,36 +126,7 @@ class _PriceScreenState extends State<PriceScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  getFinalQuoteText(),
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: Platform.isIOS ? iOSPicker() : androidDropDown(),
-          ),
-        ],
+        children: exchangeCards == null ? [doubleBounce] : exchangeCards,
       ),
     );
   }
